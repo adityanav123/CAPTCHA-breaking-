e96c2ea64d04689f6a1de471348ef3ef048a889f
Aditya Navphule <39703082+adityanav123@users.noreply.github.com>
2019-10-06 10:35:48 +0530

    Add files via upload

---
+++ captchaGenerate.py
@@ -0,0 +1,22 @@
+""""from captcha.image import ImageCaptcha 
+img = ImageCaptcha()
+image = img.generate_image('ae34bv')
+image.show()
+image.save('D:/Captcha/new_Captcha.png')"""
+
+import sys
+import os 
+import random 
+import string
+from captcha.image import ImageCaptcha
+def random_string(length = 4):
+	letters = string.ascii_lowercase
+	return ''.join(random.choice(letters) for i in range(length))
+args = sys.argv 
+directory = args[1] # where to save the generated captcha's 
+count = 1000 
+for _ in range(count):
+	img = ImageCaptcha()
+	str_to_save = random_string(4)
+	image = img.generate_image(str_to_save)
+	image.save(directory + '/' + str(str_to_save) + '.png')
